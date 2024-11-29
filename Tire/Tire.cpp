cb395055b7ba64b90efac5a2beb0acede374a5e9
Salianbooth <czc434579527@163.com>
2024-11-29 20:43:54 +0800

    前序搜索&删除操作

--- Tire/Tire.cpp
+++ Tire/Tire.cpp
@@ -9,6 +9,7 @@
 # include  <string>
 #include <map>
 #include <vector>
+#include <queue>
 
 using namespace  std;
 
@@ -17,6 +18,21 @@ public:
     //字典树的初始化
     TireTree(){
         root_ = new TireNode('\0',0);
+    }
+    ~TireTree(){
+        queue<TireNode*> que;
+        que.push(root_);
+
+        while(!que.empty()){
+                TireNode* front = que.front();
+                que.pop();
+                for(auto & pair : front->nodeMap_) {
+                    //将子节点加入队列
+                    que.push(pair.second);
+                }
+                //释放当前结点
+                delete front;
+        }
     }
 
     //添加单词串
@@ -39,6 +55,56 @@ public:
         cur->freqs_ ++;
     }
 
+    //删除单词
+    void remove(const string& word){
+        TireNode* cur = root_;
+        TireNode* del= root_;
+        char delch = word[0];
+        for(int i = 0 ; i < word.size();i++){
+            auto childIt = cur->nodeMap_.find(word[i]);
+            if(childIt == cur->nodeMap_.end()){
+                return ;
+            }
+
+            if(cur->freqs_ > 0 || cur->nodeMap_.size() > 1){
+                //有一个独立的单词
+                del = cur;
+                delch = word[i];
+            }
+            //移动到下一个结点
+            cur = childIt->second;
+        }
+        //cur指向了末尾字符结点,出来了以后，单词存在
+        //情况一 ：
+        if(cur-> nodeMap_.empty()){
+            //开始删除
+            TireNode* child = del->nodeMap_[delch];
+            del ->nodeMap_.erase(delch);
+
+            //释放相应的结点内存
+            queue<TireNode*> que;
+            que.push(child);
+
+            while(!que.empty()){
+                TireNode* front = que.front();
+                que.pop();
+                for(auto & pair : front->nodeMap_) {
+                    //将子节点加入队列
+                    que.push(pair.second);
+                }
+                //释放当前结点
+                delete front;
+            }
+
+        }
+        else{
+            //情况1
+            //当前单词末尾字符后面还有字符结点,相当于删除
+            cur-> freqs_ = 0;
+        }
+
+    }
+
     //查询单词
     int query(const string& word){
         TireNode* cur = root_;
@@ -59,11 +125,28 @@ public:
         string word;
         vector<string> wordlist ;
         preOrder(root_,word,wordlist);
-        for(auto word:wordlist){
-            cout << word << endl;
+        for(auto words:wordlist){
+            cout << words << endl;
         }
         cout << endl;
 
+    }
+    //串的前缀搜索
+    vector<string>  queryPrefix(const string& prefix){
+        TireNode* cur = root_;
+        for(int i = 0 ; i < prefix.size();i++){
+            auto childIt = cur->nodeMap_.find(prefix[i]);
+            if(childIt == cur->nodeMap_.end()){
+                return {};
+            }
+            cur = childIt->second;
+        }
+
+        //cur就指向了前缀的最后一个字符结点了
+        vector<string > wordlist;
+        string word = prefix.substr(0,prefix.size()-1);
+        preOrder(cur, word, wordlist);
+        return wordlist;
     }
 
 private:
@@ -117,6 +200,17 @@ int main(){
     cout << "ch : " <<tire.query("ch") << endl;
 
     cout << "======================" << endl;
+
+    tire.preOrder();
+
+    cout << "======================" << endl;
+
+    vector<string> wordlist = tire.queryPrefix("hell");
+    for(auto word:wordlist){
+        cout<<word << endl;
+    }
+    cout << "======================" << endl;
+    tire.remove("ch");
     tire.preOrder();
     return 0;
 }
